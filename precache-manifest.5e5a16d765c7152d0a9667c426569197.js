648d52f06d18eaea302768478ea21b8fe0c46a2f
Sacred97 <36605449+Sacred97@users.noreply.github.com>
2020-08-27 09:45:09 +0300

    Add files via upload

---
+++ precache-manifest.5e5a16d765c7152d0a9667c426569197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b635d85c0d14138da7cf395ba873dd29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4fa9f2eeee3bfec5989",
+    "url": "/static/css/main.119a255c.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fa9f2eeee3bfec5989",
+    "url": "/static/js/main.1f96ee6e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
